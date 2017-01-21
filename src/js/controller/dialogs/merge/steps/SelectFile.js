606aae6bbe710b18d47facf69208d6dce2a41bae
juliandescottes <julian.descottes@gmail.com>
2017-01-21 01:53:24 +0100

    SelectFile merge step completed

--- src/js/controller/dialogs/merge/steps/SelectFile.js
+++ src/js/controller/dialogs/merge/steps/SelectFile.js
@@ -3,6 +3,11 @@
 
   ns.SelectFile = function (piskelController, mergeController, container) {
     this.superclass.constructor.apply(this, arguments);
+
+    // Bind callbacks.
+    this.onImageFileLoaded_ = this.onImageFileLoaded_.bind(this);
+    this.onPiskelFileLoaded_ = this.onPiskelFileLoaded_.bind(this);
+    this.onPiskelFileError_ = this.onPiskelFileError_.bind(this);
   };
 
   pskl.utils.inherit(ns.SelectFile, ns.AbstractMergeStep);
@@ -32,18 +37,77 @@
   ns.SelectFile.prototype.onSelectFileChange_ = function (e) {
     var files = this.selectFileInput.files;
     if (files.length == 1) {
-      this.mergeData.mergeFile = files[0];
+      var file = files[0];
+      // Store the file in mergeData for future reference.
+      this.mergeData.mergeFile = file;
+
+      var isImage = file.type.indexOf('image') === 0;
+      var isPiskel = /\.piskel$/i.test(file.name);
+      if (isImage) {
+        pskl.utils.FileUtils.readImageFile(file, this.onImageFileLoaded_);
+      } else if (isPiskel) {
+        pskl.utils.PiskelFileUtils.loadFromFile(file,
+          this.onPiskelFileLoaded_,
+          this.onPiskelFileError_
+        );
+      }
     }
-    this.refresh_();
   };
 
   ns.SelectFile.prototype.refresh_ = function () {
-    if (!!this.mergeData.mergeFile) {
+    var mergePiskel = this.mergeData.mergePiskel;
+    if (mergePiskel) {
+      this.updateMergeFilePreview_();
+
       this.nextButton.removeAttribute('disabled');
       this.cancelButton.removeAttribute('disabled');
     } else {
       this.nextButton.setAttribute('disabled', true);
       this.cancelButton.setAttribute('disabled', true);
     }
+  };
+
+  ns.SelectFile.prototype.updateMergeFilePreview_ = function () {
+    var mergePiskel = this.mergeData.mergePiskel;
+
+    var previewFrame = pskl.utils.LayerUtils.mergeFrameAt(mergePiskel.getLayers(), 0);
+    var image = pskl.utils.FrameUtils.toImage(previewFrame);
+
+    // TODO : set background image as base64 instead.
+    this.container.querySelector('.mergefile-preview').innerHTML = '';
+    this.container.querySelector('.mergefile-preview').appendChild(image);
+
+    var metaHtml = pskl.utils.Template.getAndReplace('mergefile-meta-content', {
+      name : mergePiskel.getDescriptor().name,
+      dimensions : mergePiskel.getWidth() + ' x ' + mergePiskel.getHeight(),
+      frames : mergePiskel.getFrameCount(),
+      layers : mergePiskel.getLayers().length
+    });
+    this.container.querySelector('.mergefile-meta').innerHTML = metaHtml;
+    this.container.querySelector('.mergefile-container').classList.add('has-merge-piskel');
+  };
+
+  ns.SelectFile.prototype.onImageFileLoaded_ = function (image) {
+    var frame = pskl.utils.FrameUtils.createFromImage(image);
+    var layers = [pskl.model.Layer.fromFrames('imported', [frame])];
+    var piskel = pskl.model.Piskel.fromLayers(layers, Constants.DEFAULT.FPS, {
+      name : this.mergeData.mergeFile.name,
+      description : 'temporary piskel for merge import'
+    });
+    this.setPiskel(piskel);
+  };
+
+  ns.SelectFile.prototype.onPiskelFileLoaded_ = function (piskel) {
+    this.setPiskel(piskel);
+  };
+
+  ns.SelectFile.prototype.setPiskel = function (piskel) {
+    console.log(piskel);
+    this.mergeData.mergePiskel = piskel;
+    this.refresh_();
+  };
+
+  ns.SelectFile.prototype.onPiskelFileError_ = function () {
+    console.error("Could not load the animation");
   };
 })();
