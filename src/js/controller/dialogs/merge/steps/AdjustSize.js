1de14781185fdfa32ef99ff9d8e104b32425f0ae
juliandescottes <julian.descottes@gmail.com>
2017-01-21 12:53:52 +0100

    store merge origin in mergeData

--- src/js/controller/dialogs/merge/steps/AdjustSize.js
+++ src/js/controller/dialogs/merge/steps/AdjustSize.js
@@ -10,11 +10,27 @@
   ns.AdjustSize.prototype.init = function () {
     this.superclass.init.call(this);
     var anchorContainer = this.container.querySelector('.merge-anchor-container');
-    this.anchorWidget = new pskl.widgets.AnchorWidget(anchorContainer);
+    this.anchorWidget = new pskl.widgets.AnchorWidget(anchorContainer, this.onAnchorChange_.bind(this));
+    this.anchorWidget.setOrigin('TOPLEFT');
+  };
+
+  ns.AdjustSize.prototype.destroy = function () {
+    this.anchorWidget.destroy();
+    this.superclass.destroy.call(this);
   };
 
   ns.AdjustSize.prototype.onShow = function () {
-    this.container.querySelector('.mergefile-name').textContent =
-      this.mergeData.mergeFile.name;
+    var name = this.mergeData.mergeFile.name;
+    this.container.querySelector('.mergefile-name').textContent = name;
+
+    // TODO : Not really useful since the Step is not being removed/destroyed when
+    // transitioning out...
+    if (this.mergeData.origin) {
+      this.anchorWidget.setOrigin(this.mergeData.origin);
+    }
+  };
+
+  ns.AdjustSize.prototype.onAnchorChange_ = function (origin) {
+    this.mergeData.origin = origin;
   };
 })();
